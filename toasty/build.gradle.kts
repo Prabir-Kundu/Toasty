1c2a779b37f5740fce62247a104fc3f591fd74fe
Prabir Kundu <kundu.prabir2000@gmail.com>
2023-11-30 22:54:28 +0530

    Update gradle version "1.0.4"

--- toasty/build.gradle.kts
+++ toasty/build.gradle.kts
@@ -6,15 +6,12 @@ plugins {
     id ("maven-publish")
 }
 
-
-
 android {
     namespace = "com.prabirkundu.toasty"
     compileSdk = 34
 
     defaultConfig {
         minSdk = 24
-
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -48,44 +45,12 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
-
-/*//Publish Library Locally
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            from(components["release"])
-            groupId = "com.github.Prabir-Kundu"
-            artifactId = "Toasty"
-            version = "1.0.0"
-        }
-    }
-}*/
-
-/*//Publish Library Locally
-publishing {
-    publications {
-        release(MavenPublication) {
-            group = "com.prabirkundu.toasty"
-            artifacts = "toasty"
-            //groupId = 'com.prabirkundu.toasty'
-            //artifactId = 'dialog_x'
-            version = "1.7"
-
-            afterEvaluate {
-                from components.release
-            }
-        }
-    }
-}*/
-
 
 afterEvaluate {
     publishing {
@@ -94,22 +59,10 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.prabirkundu.toasty"
                 artifactId = "Toasty"
-                version = "1.0.3"
+                version = "1.0.4"
             }
         }
     }
 }
 
-/*afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                from(components["release"])
-                groupId = "com.github.Prabir-Kundu"
-                artifactId = "Toasty"
-                version = "1.0.1"
-            }
-        }
-    }
-}*/
+
